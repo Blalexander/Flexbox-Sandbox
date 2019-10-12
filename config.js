03c88271ada1e66b47b23663d753a922fe9616fd
Blake Alexander <blake.alexander.13@gmail.com>
2019-10-12 13:11:49 -0700

    user post error fixes

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
   "mongodb://blake:blake1@ds131903.mlab.com:31903/node-capstone";
+  // "mongodb://testuser:testpassword2@ds029824.mlab.com:29824/test-db";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/testnode_capstone";
 exports.PORT = process.env.PORT || 8080;
