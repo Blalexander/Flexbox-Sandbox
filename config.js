9b3167991b74d4e52da67a2a408819fc7616aa27
Blake Alexander <blake.alexander.13@gmail.com>
2021-01-19 11:18:40 -0800

    updated for atlas

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
-  "mongodb://blake:blake1@ds131903.mlab.com:31903/node-capstone";
+  // "mongodb://blake:blake1@ds131903.mlab.com:31903/node-capstone";
+  "mongodb+srv://blake:password222@node-capstone.3r04t.mongodb.net/node-capstone?retryWrites=true&w=majority";
   // "mongodb://testuser:testpassword2@ds029824.mlab.com:29824/test-db";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/testnode_capstone";
